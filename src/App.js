7df423ff29ac064e8e70d96075e791b892ae0031
Rick Mansfield <rick.mansfield@triplecrownproducts.com>
2022-11-04 17:29:10 -0500

    Adds Routes to App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
-
-
-
+import React from "react";
+import Navbar from "./components/Navbar";
+import { Route, Routes } from "react-router-dom";
+import Home from "./components/Home";
+import About from "./components/About";
+import Contact from "./components/Contact";
 function App() {
   return (
     <div className="App">
-      App
+      <Navbar />
+      <div>
+        <Routes>
+          <Route path="/" exact element={<Home />} />
+          <Route path="/Home" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+      </div>
     </div>
   );
 }
