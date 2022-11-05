03cb965fb02bc2977a3fd9d3887f15cb7447bef3
Rick Mansfield <rick.mansfield@triplecrownproducts.com>
2022-11-05 08:08:11 -0500

    Refactors Project Index for Redux State Management

--- src/index.js
+++ src/index.js
@@ -1,12 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import React from "react";
 import { BrowserRouter as Router } from "react-router-dom";
-import App from './App';
-
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
+import App from "./App";
+import { Provider } from "react-redux";
+import store from "./store/storeIndex";
+import { createRoot } from "react-dom/client";
+const container = document.getElementById("root");
+const root = createRoot(container);
 root.render(
-  <Router>
-    <App />
-  </Router>
+  <Provider store={store}>
+    <Router basename={process.env.PUBLIC_URL}>
+      <App />
+    </Router>
+  </Provider>,
+  
 );
+
