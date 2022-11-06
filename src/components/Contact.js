f583cd042b836ef5f7878eb6a137606865e56993
Rick Mansfield <rick.mansfield@triplecrownproducts.com>
2022-11-06 15:19:36 -0600

    Demos <Modal /> in Contacts.js

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,8 +1,9 @@
 import React, { useEffect } from "react";
 import { useNavigate, Link } from "react-router-dom";
 // import { useParams } from "react-router-dom";
-
+import Modal from "./Modal";
 function Contact() {
+
   // let navigate = useNavigate();
   // setTimeout(() => {
   //   navigate('/about');
@@ -14,6 +15,8 @@ function Contact() {
   // }, [params.id]);
   return (
     <div className="Contacts">
+      <Modal />
+
       <div
         className="ui rasied very padded text container segment"
         style={{ marginTop: "80px" }}
