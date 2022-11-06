9f9a0e2c138d01af1817c1b15bab52eecea05578
Rick Mansfield <rick.mansfield@triplecrownproducts.com>
2022-11-05 21:01:27 -0500

    Creates new modal component

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,20 +1,19 @@
-import React from 'react';
-import { connect } from 'react-redux';
-import ReactDOM from 'react-dom';
+import React from "react";
+import { connect } from "react-redux";
+import ReactDOM from "react-dom";
 
 export const Modal = (props) => {
-
   return ReactDOM.createPortal(
-    <div className='ui dimmer show modal visible active'>
-      <div className='ui raised very padded text container'>
-      Modal
+    <div className="ui dimmer show modal visible active">
+      <div className="ui raised very padded text container">
+        <h1>Modal</h1>
       </div>
-    </div>
-  )
-}
+    </div>, document.querySelector("#modal")
+  );
+};
 
-const mapStateToProps = (state) => ({})
+const mapStateToProps = (state) => ({});
 
-const mapDispatchToProps = {}
+const mapDispatchToProps = {};
 
-export default connect(mapStateToProps, mapDispatchToProps)(Modal)
+export default connect(mapStateToProps, mapDispatchToProps)(Modal);
