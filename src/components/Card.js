c97239aee438a18f3f9530f71764dfd11ae9d195
Rick Mansfield <rick.mansfield@triplecrownproducts.com>
2022-11-07 14:21:05 -0600

    Connects to Redux Store 4 Dynamic use

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,28 +1,32 @@
-import React, { useEffect, useState } from "react";
-import { useParams } from 'react-router-dom';
-import { connect } from 'react-redux';
+import React from "react";
+import { useParams } from "react-router-dom";
+import { connect} from 'react-redux';
 
-function Card() {
-  const [user, setUser] = useState("");
-  const params = useParams();
 
-  useEffect(() => { 
-    setUser(params.user);
-  }, [ params.user ]);
+function Card(props) {
+  console.log('props in Card', props);
+  let id = useParams();
+  console.log('id', id);
+  
 
+  const card = props.cards.find((card) => card.id === parseInt(id.id));
+  
   return (
     <div className='ui raised very padded text container segment'
     style={{marginTop: '80px'}}>
       <h3 className='ui header'>
-      {user}
+        {card.title}
       </h3>
+      <p>
+        {card.description}
+      </p>
     </div>
   )
-}
+};
+
 const mapStateToProps = (state) => ({
   state: state,
-  user: state.user,
+  cards: state.rootReducer.cards,
 });
-
 
 export default connect(mapStateToProps)(Card)
